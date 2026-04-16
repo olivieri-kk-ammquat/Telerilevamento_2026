0ebb89723635aab83fa3b274a07471f2885fb6fd
Chiara Olivieri <olivieri.kk@gmail.com>
2026-04-16 15:43:59 +0200

    Update 06_exam_simulation.R

--- Code/06_exam_simulation.R
+++ Code/06_exam_simulation.R
@@ -1,1 +1,23 @@
 
+library(terra) #to import
+
+setwd("C:/Users/Utente/Desktop") #cartella di riferimento
+
+getwd()
+list.files()
+
+richat <- rast("richatstructure_oli_20260306.jpg")
+richat <- flip(richat)
+plot(richat)
+
+png("figura.png")
+plot(richat)
+dev.off()
+
+png("bande.png")
+im.multiframe(2,1)
+plot(richat[[1]])
+plost(richat[[2]])
+dev.off()
+-----------
+
