db70730b7b6af05713f8b440d6767a05c45582bd
Chiara Olivieri <olivieri.kk@gmail.com>
2026-04-23 15:01:40 +0200

    Create 08.functions.R

---
+++ Code/08.functions.R
@@ -0,0 +1,19 @@
+#how to develop your own function
+#which are the topics of my function? x and y, resulting in z which is defined as an output by return
+
+somma <- function(x,y){  
+z=x+y
+return(z)
+  }
+
+ #now I can use my function
+somma(15,18)
+
+#let's try another
+
+differenza <- function(x,y) {
+z=x-y
+return(z)
+  }
+
+#how to use if and else 
